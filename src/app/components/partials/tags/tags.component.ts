f5e69de5ba4420e0cf42be92f9bb001f9cf1a762
doaahafez <dhafez842@gmail.com>
2023-06-07 17:57:11 +0300

    Doaa Added Folder

---
+++ src/app/components/partials/tags/tags.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Tag } from '../../../shared/models/Tags';
+import { FoodService } from 'src/app/services/food.service';
+// import { BookingService } from '../booking.service';
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.scss']
+})
+export class TagsComponent {
+  tags?:Tag[];
+  constructor(foodService:FoodService){
+    this.tags = foodService.getAllTags();
+  }
+}
